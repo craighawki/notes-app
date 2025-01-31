6244c5602de53c4def2b0e72021142ce5e1ade4a
Craig Hawkins <craighawki@gmail.com>
2025-01-30 22:42:44 -0800

    c

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const note = getNotes('This is my fugging note!')
 console.log(note)
 
-console.log(validator.isURL('https://adobe.com'))
+console.log(chalk.blue('Blue Chalk'))
 
+console.log(chalk.bold.inverse.green('Blue Chalk'))
+console.log(chalk.bold.strikethrough.italic.bgWhite.red('ERROR ERROR ERROR!!!!'))
+console.log(chalk.inverse.dim.underline.magentaBright('Almost sprintime'))
